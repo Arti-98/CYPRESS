934aaedb565052d29b9032ab33798a8914a776a7
Arti-98 <chamlearti95@gmail.com>
2025-05-07 13:23:02 +0530

    LoginTest.cy.js

--- LoginTest.cy.js
+++ LoginTest.cy.js
@@ -1,12 +1,15 @@
-import LoginPage from '..\..\PageObjects\LoginPage.js';
-describe('OrangeHRM Login Test', () => {
+import LoginPage from '../support/pageObjects/LoginPage';
+
+describe('Login Test', () => {
   const loginPage = new LoginPage();
 
   it('should log in with valid credentials', () => {
-    loginPage.visit();
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
     loginPage.enterUsername('Admin');
     loginPage.enterPassword('admin123');
     loginPage.clickLogin();
+
     cy.url().should('include', '/dashboard');
   });
 });
